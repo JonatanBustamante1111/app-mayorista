221759dee0f6a669158583a7862b5674b5842742
jonatanbustamante1111 <jonatanbustamante1111@gmail.com>
2023-03-18 15:14:24 -0300

    navbar fail

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import Nav from "./Nav";
+
+
+const NavBar = () => {
+    // const theme = {
+    //   colors: {
+    //     footer_bg: "#0a1435",
+    //     btn: "rgb(98 84 243)",
+    //     border: "rgba(98,84,243,0.5)",
+    //     hr:"#fffff",
+    //     gradient:
+    //     "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 253) 100%)",
+    //     shadow: "rgba(0,0,0,0.02) 0px 1px 3px 0px, rgba(27,31,35,0.15 )",
+    //     shadowSupport:"rgba(0,0,0,0.16) 0px 1px 4px",
+    //   },
+    //   media: {
+    //     mobile: "768px",
+    //     tab: "998px",
+    //   },
+    // };
+  return (
+    <Nav></Nav>
+  );
+};
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
+const router = createBrowserRouter ([
+  {
+    path: '/',
+    element: <NavBar />
+  },
+  {
+    path: '/nosotros',
+    element: <h2>prueba 2</h2>
+  },
+  {
+    path: '/contacto'
+  } 
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
