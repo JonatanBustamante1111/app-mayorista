d0beab057c15ec198dfad97767a353b286fbaf30
jonatanbustamante1111 <jonatanbustamante1111@gmail.com>
2023-03-19 11:20:24 -0300

    guardo cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import NavBar from './components/NavBar'
 
 const router = createBrowserRouter ([
   {
     path: '/',
-    element: <NavBar />
   },
   {
     path: '/nosotros',
@@ -20,5 +18,5 @@ const router = createBrowserRouter ([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
