547be50775ca25b2f7235e8c41fdb53db3d0ac61
jonatanbustamante1111 <jonatanbustamante1111@gmail.com>
2023-03-19 13:26:54 -0300

    merge rama agus

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,20 @@ import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import Layout from './components/Layout'
+import Inicio from './pages/Inicio'
+
 
 const router = createBrowserRouter ([
   {
     path: '/',
-    element: <NavBar />
+    element: <Layout/>,
+    children: [
+      {
+        index:true,
+        element:<Inicio/>
+      }
+    ]
   },
   {
     path: '/productos',
@@ -20,5 +28,5 @@ const router = createBrowserRouter ([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
