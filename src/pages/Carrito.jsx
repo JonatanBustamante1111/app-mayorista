c86a36a85e73a0f1677c243b2c934362990ef562
jonatanbustamante1111 <jonatanbustamante1111@gmail.com>
2023-03-26 14:25:48 -0300

    si hay producto en el carrito muestro una interfazC.C muestro otra

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -3,27 +3,31 @@ import TablaCarrito from "../components/TablaCarrito";
 import { CartContext } from "../context/CartContext";
 
 export default function Carrito() {
-   const {cart} = useContext(CartContext)
+  const { cart } = useContext(CartContext);
 
-    return(
+  return (
+    <>
+      {cart.carrito.length ? (
         <table>
-        <thead>
-          <tr>
-            <th>Producto</th>
-            <th>Cantidad</th>
-            <th>Precio unitario</th>
-            <th>Total</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            cart.carrito.length > 0
-            && cart.carrito.map(
-              producto => <TablaCarrito key={producto.id} producto={producto}/>
-            )
-          }
-        </tbody>
-      </table>
-    )
+          <thead>
+            <tr>
+              <th>Producto</th>
+              <th>Cantidad</th>
+              <th>Precio unitario</th>
+              <th>Total</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.carrito.length > 0 &&
+              cart.carrito.map((producto) => (
+                <TablaCarrito key={producto.id} producto={producto} />
+              ))}
+          </tbody>
+        </table>
+      ) : (
+        <div>Tu carrito esta vacio</div>
+      )}
+    </>
+  );
 }
