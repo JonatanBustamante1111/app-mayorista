41e738f634e9fb2dc6be66503760ec3a682c5b7d
agustinnarciande <narciande.2019@gmail.com>
2023-04-06 12:31:41 -0300

    Contact Page Done

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -3,15 +3,15 @@ import ContactoForm from "../components/ContactoForm";
 
 export default function Contacto() {
   return (
-    <div className="flex flex-row justify-between">
-        <ContactoForm/>
-      <div>
+    <main className="grid md:grid-cols-2 md:mt-12 place-items-center">
+      <ContactoForm />
+      <section>
         <img
           src="../public/imageDesktopContacto.png"
           alt="Mina Facherarda"
-          className='w-full h-full transform scale-x-[-1] scale-y-[1] '
+          className='md:w-[450px] md:h-[400px]  lg:w-[550px] lg:h-[489px] xl:w-[771px] xl:h-[724px] transform scale-x-[-1] scale-y-[1] '
         />
-      </div>
-      </div>
+      </section>
+    </main>
   );
 }
