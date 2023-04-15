5f43bfad66f8d3b3a5b39fb45f8b83c1bfae2423
cxg346 <dgm3036@gmail.com>
2023-04-15 15:46:12 -0500

    link final

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const eApi = axios.create({
-    baseURL: 'https://07ac-181-67-104-89.ngrok-free.app/api/mp/'
+    baseURL: 'http://crv4mayorista.com/api/mp/'
 })
 
 export default eApi;
