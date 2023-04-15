0412c1b0ba262098edd843af7b52e60d2decb32f
cxg346 <dgm3036@gmail.com>
2023-04-15 14:45:00 -0500

    default export

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export const eApi = axios.create({
+const eApi = axios.create({
     baseURL: 'https://07ac-181-67-104-89.ngrok-free.app/api/mp/'
 })
+
+export default eApi;
