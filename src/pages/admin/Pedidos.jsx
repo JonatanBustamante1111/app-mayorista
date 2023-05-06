12e64e59b26bc6a89490ec577e4fcc95836cf07f
agustinnarciande <narciande.2019@gmail.com>
2023-05-06 17:30:04 -0300

    Solamente se renderizan los pedidos aprovados por mercado pago

--- src/pages/admin/Pedidos.jsx
+++ src/pages/admin/Pedidos.jsx
@@ -11,20 +11,20 @@ const Pedidos = () => {
     useEffect(() => {
         const docRef = collection(db, "pedidosCliente");
         const unsubscribe = onSnapshot(docRef, (snapshot) => {
-          const pedidos = snapshot.docs.map( doc => {
-            return {
-              id: doc.id,
-              ...doc.data(),
-            };
-          });
-          setPedidos(pedidos);
+            const pedidos = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                };
+            });
+            setPedidos(pedidos);
         });
         return () => unsubscribe();
-      }, []);
+    }, []);
     return (
         <main className='w-[75%] ml-[25%]'>
             <section>
-                
+
                 <article className="w-[90%] mt-20 mb-5 mx-auto grid grid-cols-[2fr,2fr,1fr,1fr,1fr] gap-x-10 place-items-start self-center">
                     <p className="text-lg font-medium text-blanco">Numero de pedido</p>
                     <p className="text-lg font-medium text-blanco">Nombre</p>
@@ -34,12 +34,11 @@ const Pedidos = () => {
                 </article>
             </section>
             <section className="flex flex-col  h-3/4  overflow-auto ">
-                {
-
-                    pedidos.map(pedido =>
-                        <Pedido key={pedido.id} pedido={pedido} />
-                    )
-                }
+                {pedidos.map(pedido => (
+                    pedido.status === 'approved'
+                        ? <Pedido key={pedido.id} pedido={pedido} />
+                        : null
+                ))}
             </section>
         </main>
     )
